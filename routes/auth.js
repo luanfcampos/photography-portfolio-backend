73b74a64feaf2f0231cdef714ff982e2e77b0e75
Luan <luan@cnnct.black>
2025-08-21 02:53:52 -0300

    fix: corrige import do módulo postgres no auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { getDatabase } = require('../postgres');
+const { getDatabase } = require('../database/postgres');
 
 const router = express.Router();
 
